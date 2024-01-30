531987559a6a85cc9994370cbc2c9986e578e80b
David Changea <14ir155@gmail.com>
2024-01-30 16:59:53 +0100

    navactive mis en place sur les liens de la nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import homeBack from "../assets/img/background-1.jpg";
 
 const HomePage = () => {
-  return <div></div>;
+  return <div className="home"></div>;
 };
 
 export default HomePage;
